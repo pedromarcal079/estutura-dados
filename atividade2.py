22195b6de4bd2306380326ea8b7e3b663e5183bd
Pedro William <pedro.marcal.079@ufrn.edu.br>
2026-03-06 16:14:47 -0300

    Atividade 2

--- atividade2.py
+++ atividade2.py
@@ -1,12 +1,12 @@
 import re
+import datetime
 
 print(" ### Cadastramento de usuário ###")
 
 nome = input("Insira seu nome: ")
 senha = input("Insira sua senha: ")
 email = input("Insira seu email: ")
-data_nasc = input("Insira sua data de nascimento: ")
-
+# Créditos ao site GeeksForGeeks em: Check if Email Address Valid or not in Python
 def valida_email(email):
     regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$'
     if re.match(regex, email):
@@ -18,4 +18,18 @@ if valida_email(email):
     print("Email válido")
 else:
     print("Email invalido")
+    
+data_nasc = input("Insira sua data de nascimento: ")
 
+dia = int(data_nasc[0:2])
+mes = int(data_nasc[3:5])
+ano = int(data_nasc[6:10])
+
+# Créditos ao site GeeksForGeeks em: Python DateTime - Date Class
+data = datetime.date(ano,mes,dia)
+
+print("\n## Sucesso!")
+print("## Nome :",nome)
+print("## Senha :",senha)
+print("## Email :",email)
+print("## Data de nascimento :",data)
