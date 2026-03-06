dbf84c24fc530fce55f8844bd880e12a97498085
Pedro William <pedro.marcal.079@ufrn.edu.br>
2026-03-06 15:24:16 -0300

    Pedro William - Atividade 1

---
+++ atividade1.py
@@ -0,0 +1,10 @@
+"""
+"""
+print(" ### Cadastramento de usuário ###")
+
+nome = input("\nInsira seu nome: ")
+idade = int(input("Insira sua idade: "))
+
+print("\n## Sucesso!")
+print("## Nome :",nome)
+print("## Idade :",idade)
