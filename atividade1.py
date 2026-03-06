7426d9f7f4acdbd05bf832dc4c8c4a4e4242a628
Luisfelipelinhares <luis.felipe.linhares.701@ufrn.edu.br>
2026-03-06 19:21:57 +0000

    atividade 2

--- atividade1.py
+++ atividade1.py
@@ -1,10 +1,28 @@
-"""
-"""
-print(" ### Cadastramento de usuário ###")
+import re
+import datetime
 
-nome = input("\nInsira seu nome: ")
-senha = int(input("Insira sua senha: "))
 
-print("\n## Sucesso!")
-print("## Nome :",nome)
-print("## Idade :",senha)
+
+print('###  cacdastro de usuario ###\n')
+
+nome = input('Digite o nome do usuario\n')
+senha = input('Digite a idade do usuario\n')
+
+
+data_nasc = input('Digite a data de nascimento (dd/mm/aa):\n')
+
+dia = int(data_nasc[0:2])
+mes = int(data_nasc[3:5])
+ano = int(data_nasc[6:10])
+
+data_nasc = datatime.data(ano,mes,dia)
+    
+# em desenvolvimento
+#email = input('Digite o email do usuario\n')
+
+
+print('##Cadastro realizado com sucesso!##\n')
+
+print('nome:', nome,)
+print('idade:', senha)
+print('Data de nascimento:', data_nasc)
